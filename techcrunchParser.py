f8d9ab905b211d1dc020eca00e75c9df74e72ae2
yaowenc2 <yaowenc2@illinois.edu>
2024-04-11 22:38:47 -0500

    Add tech crunch parser

---
+++ techcrunchParser.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://techcrunch.com/'
+
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+items = soup.find_all('h2', class_='post-block__title') + soup.find_all('h3', class_='mini-view__item__title')
+
+parsed_items = [[item.text.strip(), item.a['href']] for item in items]
+    
+for item in parsed_items:
+    print(item)
