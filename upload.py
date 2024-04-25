6ed806b5b6d37f81346f47296f31d8aecbade515
yaowenc2 <yaowenc2@illinois.edu>
2024-04-25 18:43:41 -0500

    Updated selenium

--- upload.py
+++ upload.py
@@ -12,74 +12,94 @@ load_dotenv()
 url = "https://podcasters.spotify.com/pod/dashboard/home"
 email= os.getenv("EMAIL")
 password = os.getenv("PASSWORD")
+title_dict = {
+    "English": "AI Briefing Room",
+    "Spanish": "Sala de Información de IA - AI Briefing Room",
+    "Chinese": "AI简报室 - AI Briefing Room",
+}
 
-webdriver_service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=webdriver_service)
-driver.get(url)
+def upload_podcast(language= "English", date = "2024-04-24", title = "AI Briefing Room", description = "AI Briefing Room Description"):
+    webdriver_service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=webdriver_service)
+    driver.get(url)
 
-# Click the "Continue with Spotify" button
-continue_with_spotify_button =  WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[span[contains(text(), "Continue with Spotify")]]')))
-continue_with_spotify_button.click()
+    # Click the "Continue with Spotify" button
+    continue_with_spotify_button =  WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[span[contains(text(), "Continue with Spotify")]]')))
+    continue_with_spotify_button.click()
 
-# Log in
-email_field =  WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "login-username")))
-password_field = driver.find_element(By.ID, "login-password")
-email_field.send_keys(email)
-password_field.send_keys(password)
-login_button = driver.find_element(By.ID, "login-button")
-login_button.click()
+    # Log in
+    email_field =  WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "login-username")))
+    password_field = driver.find_element(By.ID, "login-password")
+    email_field.send_keys(email)
+    password_field.send_keys(password)
+    login_button = driver.find_element(By.ID, "login-button")
+    login_button.click()
 
-# Continue to App
-continue_to_app_button = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.XPATH, "//*[@id='encore-web-main-content']/div/main/section/div/div/button/span[1]"))
+    # Continue to App
+    continue_to_app_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//*[@id='encore-web-main-content']/div/main/section/div/div/button/span[1]"))
+    )
+    continue_to_app_button.click()
+    time.sleep(5)
+    
+    '''
+    # This gets the which podcast for us
+    first_div = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, '//body/div'))
 )
-continue_to_app_button.click()
-time.sleep(5)
+    # Print the text of the first 'div' element
+    first_text = first_div.text.split("\n")[0]
+    print(f"Text of the first div: {first_text}")
+    '''
+    
+    # new episode
+    driver.get("https://podcasters.spotify.com/pod/dashboard/episode/wizard")
+    time.sleep(5)
 
-# new episode
-current_url = driver.current_url
-driver.get("https://podcasters.spotify.com/pod/dashboard/episode/wizard")
-time.sleep(5)
+    # upload file
+    file_path = f"output/{date}/{language}_final_podcast.mp3"
+    current_dir = os.getcwd()  
+    absolute_path = os.path.join(current_dir, file_path)
+    file_input = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//input[@type='file']"))
+    )
+    file_input.send_keys(absolute_path)
+    time.sleep(3)
 
-# upload file
-file_path = "output/2024-04-24/English_final_podcast.mp3"
-current_dir = os.getcwd()  
-absolute_path = os.path.join(current_dir, file_path)
-file_input = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH, "//input[@type='file']"))
-)
-file_input.send_keys(absolute_path)
-time.sleep(3)
+    # Details
+    title_input = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.NAME, "title"))  
+    )
+    title_input.send_key(title)
 
-# Details
-title_input = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.NAME, "title"))  
-)
-title_input.send_keys('Your Podcast Title')
+    description_input = driver.find_element(By.NAME, "description")
+    description_input.send_keys(description)
 
-description_input = driver.find_element(By.NAME, "description")
-description_input.send_keys('Your Podcast Description')
+    publish_now_radio = driver.find_element(By.ID, "publish-date-now")
+    driver.execute_script("arguments[0].click();", publish_now_radio)
+    no_explicit_content_radio = driver.find_element(By.ID, "no-explicit-content")
+    driver.execute_script("arguments[0].click();", no_explicit_content_radio)
+    no_promotional_radio = driver.find_element(By.ID, "no-sponsored-content")
+    driver.execute_script("arguments[0].click();", no_promotional_radio)
+    time.sleep(5)
+    footer = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.TAG_NAME, "footer"))
+    )
+    driver.execute_script("arguments[0].scrollIntoView();", footer)
+    next_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//button[.//span[text()='Next']]"))
+    )
+    next_button.click()
+    time.sleep(3)
 
-publish_now_radio = driver.find_element(By.ID, "publish-date-now")
-driver.execute_script("arguments[0].click();", publish_now_radio)
-no_explicit_content_radio = driver.find_element(By.ID, "no-explicit-content")
-driver.execute_script("arguments[0].click();", no_explicit_content_radio)
-no_promotional_radio = driver.find_element(By.ID, "no-sponsored-content")
-driver.execute_script("arguments[0].click();", no_promotional_radio)
-time.sleep(5)
-footer = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.TAG_NAME, "footer"))
-)
-driver.execute_script("arguments[0].scrollIntoView();", footer)
-next_button = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.XPATH, "//button[.//span[text()='Next']]"))
-)
-next_button.click()
-time.sleep(3)
+    # Interact
+    next_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//button[.//span[text()='Next']]"))
+    )
+    next_button.click()
+    time.sleep(3)
+    
 
-# Interact
-next_button = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.XPATH, "//button[.//span[text()='Next']]"))
-)
-next_button.click()
-time.sleep(3)
+
+
+upload_podcast("English", "2024-04-24", "AI Brefing Room", "AI Briefing Room Description")
