d4d8b53138b1374194138c4b9bc242485c147c8e
Xiao215 <xiaozhang20030215@gmail.com>
2024-05-19 23:02:33 -0700

    Fix newsletter issue and add it to the main pipeline (might be removed later)

--- newsLetter/newsletter.py
+++ newsLetter/newsletter.py
@@ -5,16 +5,19 @@ from email.mime.text import MIMEText
 from openai import OpenAI
 
 from dotenv import load_dotenv
+from pathlib import Path
+current_dir = Path(__file__).parent
+parent_dir = current_dir.parent
 
 
 TLDR_EXAMPLE = ""
-with open("newsletter.md", 'r', encoding='utf-8') as file:
+with open(current_dir / "newsletter.md", 'r', encoding='utf-8') as file:
     TLDR_EXAMPLE = file.read()
-    
+
 TLDR_TITLE_EXAMPLE = ""
-with open("newsletter_title.md", 'r', encoding='utf-8') as file:
+with open(current_dir / "newsletter_title.md", 'r', encoding='utf-8') as file:
     TLDR_TITLE_EXAMPLE = file.read()
-    
+
 MODEL = "GPT4"
 
 load_dotenv()
@@ -23,7 +26,7 @@ client = OpenAI(
     api_key = os.environ.get("OPENAI_API_KEY"),
     organization = os.getenv('OPENAI_ORGANIZATION_ID')
 )
-def generate_newsletter_title(content):
+def generate_newsletter_title(content:str)->str:
     role = f"""
     You are a professional newsletter title generator specialized in technology topics. Your task is to generate the overall title based on the newsletter. The title of the newsletter should be 1 line only, easy to read, include bullet points for key facts. Use a combination of emoji.
     You can reference this example but style it with markdown {{TLDR_TITLE_EXAMPLE}}. You must generate only 1 line title because the title should be concise enough."""
@@ -44,11 +47,11 @@ def generate_newsletter_title(content):
         )
         return completion.choices[0].message
     except Exception as e:
-        
+
         print(f"Error while generating newsletter title: {e}")
         return None
-    
-def generate_newsletter(content):
+
+def generate_newsletter(content:str)->str:
     # role = f"You are a helpful podcast content summarizer. You need to summarize the podcast and decorate it in Markdown. \n\n You can reference this example but style it with markdown: \n\n{TLDR_example}\n"
     role = f"""You are a professional newsletter editor specialized in technology topics. Your task is to summarize the provided content into a concise, engaging, and informative markdown newsletter. The newsletter should be easy to read, include bullet points for key facts, subheadings for different sections, and incorporate a formal yet engaging tone. Use hyperlinks appropriately to encourage readers to engage further. Use combination of emoji. \n\n You can reference this example but style it with markdown {TLDR_EXAMPLE}"""
 
@@ -70,11 +73,11 @@ def generate_newsletter(content):
         )
         return completion.choices[0].message
     except Exception as e:
-        
+
         print(f"Error while generating newsletter title: {e}")
         return None
 
-def send_email(subject, message, to_email, is_markdown=False):
+def send_email(subject:str, message:str, to_email, is_markdown=False)->None:
     if message is None:
         print("No newsletter content to send.")
         return
@@ -108,7 +111,7 @@ def send_email(subject, message, to_email, is_markdown=False):
 
 import sqlite3
 # Database functions
-def init_db():
+def init_db() -> None:
     conn = sqlite3.connect('subscribers.db')
     c = conn.cursor()
     c.execute('''
@@ -119,7 +122,7 @@ def init_db():
     conn.commit()
     conn.close()
 
-def add_subscriber(email):
+def add_subscriber(email:str) -> None:
     conn = sqlite3.connect('subscribers.db')
     c = conn.cursor()
     try:
@@ -129,7 +132,7 @@ def add_subscriber(email):
         print("Email already exists in the database.")
     conn.close()
 
-def get_subscribers():
+def get_subscribers() -> list[str]:
     conn = sqlite3.connect('subscribers.db')
     c = conn.cursor()
     c.execute('SELECT email FROM subscribers')
@@ -161,48 +164,57 @@ def get_subscribers(service):
     rows = result.get('values', [])
     return [row[0] for row in rows if row]
 
-def format_newsletter(content):
+def format_newsletter(content: str)->tuple[str, str]:
     newsletter_content = generate_newsletter(content).content
     newsletter_content = "###" + "###".join(newsletter_content.split("###")[1:]).split("---")[0] # remove title and intro, and summary at the end
     title = generate_newsletter_title(newsletter_content).content.lstrip("#")
     newsletter_content += "\n\n[🔊 Listen to the Full Podcast Episode Here](https://podcasters.spotify.com/pod/show/aibriefingroom)"
-    with open('newsletter_content_testing.md', 'w', encoding='utf-8') as file:
-        file.write(newsletter_content)
-    with open('newsletter_title_testing.md', 'w', encoding='utf-8') as file:
-        file.write(title)
+    return title, newsletter_content
+    # with open('newsletter_content_testing.md', 'w', encoding='utf-8') as file:
+    #     file.write(newsletter_content)
+    # with open('newsletter_title_testing.md', 'w', encoding='utf-8') as file:
+    #     file.write(title)
 
-def send_newsletter(use_sheet = True):
-    if use_sheet:
+def send_newsletter(newsletter_content:str, title:str, use_sheet = True, test = False) -> None:
+    if test:
+        subscribers = ['1835928575qq@gmail.com'] # testing purpose
+    elif use_sheet:
       service = google_sheets_service()
       subscribers = get_subscribers(service)
     else:
       init_db()
       subscribers = get_subscribers()  # Retrieve all subscribers
-    # subscribers = ['jc12020@nyu.edu'] testing purpose
-    with open('newsletter_content_testing.md', 'r', encoding='utf-8') as file:
-        newsletter_content = file.read()
 
-    with open('newsletter_title_testing.md', 'r', encoding='utf-8') as file:
-        title = file.read()
+
+    # with open('newsletter_content_testing.md', 'r', encoding='utf-8') as file:
+    #     newsletter_content = file.read()
+
+    # with open('newsletter_title_testing.md', 'r', encoding='utf-8') as file:
+    #     title = file.read()
 
     for email in subscribers:
         send_email(title, newsletter_content, email, is_markdown=True)
         print(email)
 
-def extract_podcast_description(file_path):
+def extract_podcast_description(file_path: str|None=None, content: str|None = None) -> str:
     import re
-    with open(file_path, 'r', encoding='utf-8') as file:
-        content = file.read()
-    
+    if file_path is not None:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+
+    if content is None:
+        raise ValueError("Either file_path or content should be provided.")
+
     # Use regex to find the podcast description
     match = re.search(r"Podcast Description:\n(.*?)\nPolished Script \(Spanish\):", content, re.DOTALL)
     if match:
         podcast_description = match.group(1).strip()
         return podcast_description
     else:
-        return None   
+        raise ValueError("Podcast description not found in the content.")
+
 if __name__ == "__main__":
-    # Path to your file, should keep it updated
-    file_path = "../output/2024-05-16/podcast_data.txt"
-    format_newsletter(content = extract_podcast_description(file_path))
-    send_newsletter(use_sheet = True)
+#     # Path to your file, should keep it updated
+    file_path = parent_dir / "output/2024-05-19/podcast_data.txt"
+    title, content = format_newsletter(extract_podcast_description(file_path=file_path))
+    send_newsletter(content, title, use_sheet=True, test=True)
